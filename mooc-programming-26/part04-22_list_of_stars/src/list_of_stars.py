f0d4ab44781ccc8f32e149012998119c2758bbc8
Rozina Karki <rozina.karki07@gmail.com>
2026-08-16 16:41:25 +0545

    second and final update to list of stars

--- mooc-programming-26/part04-22_list_of_stars/src/list_of_stars.py
+++ mooc-programming-26/part04-22_list_of_stars/src/list_of_stars.py
@@ -1,12 +1,9 @@
-def list_of_stars():
-    # .split() takes user input like "3 1 5" and turns it into a list: ['3', '1', '5']
-    my_list = input("Please enter numbers separated by space: ").split()
-
+def list_of_stars(my_list):
     i = 0
     while i < len(my_list):
-        num = int(my_list[i])  
-        print("*" *num)
+        print("*" * my_list[i])
         i = i + 1
 
 
-list_of_stars()
+if __name__ == "__main__":
+    list_of_stars([3, 7, 10])
