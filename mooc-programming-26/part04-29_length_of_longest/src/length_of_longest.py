564f6f08eedc083aaf1f8fe4212d7a728f401fd0
Rozina Karki <rozina.karki07@gmail.com>
2026-08-21 22:41:26 +0545

    added length of longest.py

--- mooc-programming-26/part04-29_length_of_longest/src/length_of_longest.py
+++ mooc-programming-26/part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,11 @@
-# Write your solution here
+def length_of_longest(my_list):
+    best= ""
+    for item in my_list:
+        if len(item)>len(best):
+            best = item
+    return len(best)
+if __name__ == "__main__":
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+    result = length_of_longest(my_list)
+    print(result)
