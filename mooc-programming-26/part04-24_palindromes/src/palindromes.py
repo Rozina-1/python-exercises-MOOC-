2f3ed20882eba77e233c771ec4d1af02b3ab9960
Rozina Karki <rozina.karki07@gmail.com>
2026-08-21 10:48:22 +0545

    added palindromes

--- mooc-programming-26/part04-24_palindromes/src/palindromes.py
+++ mooc-programming-26/part04-24_palindromes/src/palindromes.py
@@ -1,4 +1,12 @@
-# Write your solution here
-# Note, that at this time the main program should not be written inside
-# if __name__ == "__main__":
-# block!
+def palindromes(str2):
+    if(str2[::-1]==str2):
+        return True
+    else:
+        return False
+while(True):
+    str1 = input("Please type in a palindrome: ")
+    if(palindromes(str1)):
+        print(str1,"is a palindrome!")
+        break
+    else:
+        print("that wasn't a palindrome")
