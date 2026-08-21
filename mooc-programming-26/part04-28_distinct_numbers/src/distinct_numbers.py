cf16d8ee5d9aa8759a4619013a2b208a07fe32a6
Rozina Karki <rozina.karki07@gmail.com>
2026-08-21 22:35:29 +0545

    Added distinct number.py

--- mooc-programming-26/part04-28_distinct_numbers/src/distinct_numbers.py
+++ mooc-programming-26/part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,11 @@
-# Write your solution here
+def distinct_numbers(my_list):
+    unique_list = []
+    
+    for item in my_list:
+        if item not in unique_list:
+            unique_list.append(item)
+            
+    return sorted(unique_list)
+if __name__ == "__main__":
+    my_list = [3, 2, 2, 1, 3, 3, 1]
+    print(distinct_numbers(my_list))  
