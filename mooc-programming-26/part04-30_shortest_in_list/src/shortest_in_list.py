24e39c566335e598c076fde9ca8789db7bc69cbf
Rozina Karki <rozina.karki07@gmail.com>
2026-08-21 22:47:55 +0545

    added shortest in list

--- mooc-programming-26/part04-30_shortest_in_list/src/shortest_in_list.py
+++ mooc-programming-26/part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def shortest(my_list):
+    best = my_list[0]
+    for item in my_list:
+        if len(item)<len(best):
+            best = item
+    return best
+if __name__ == "__main__":
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+    result = shortest(my_list)
+    print(result)
