06a75cef5cba286b1aa3069eb9482cf82c286ca0
Rozina Karki <rozina.karki07@gmail.com>
2026-08-21 10:14:37 +0545

    Added anagrams

--- mooc-programming-26/part04-23_anagrams/src/anagrams.py
+++ mooc-programming-26/part04-23_anagrams/src/anagrams.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def anagrams(str1,str2):
+    if(sorted(str1)==sorted(str2)):
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    str1 = input("Enter a string: ")
+    str2 = input("Ener another string: ")
+    print(anagrams(str1,str2))
