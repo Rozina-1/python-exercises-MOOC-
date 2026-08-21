9e42ada8eff50bcd538963defefc431e94258c18
Rozina Karki <rozina.karki07@gmail.com>
2026-08-21 22:11:48 +0545

    Added sum of lists

--- mooc-programming-26/part04-27_sum_of_lists/src/sum_of_lists.py
+++ mooc-programming-26/part04-27_sum_of_lists/src/sum_of_lists.py
@@ -1,1 +1,10 @@
-# Write your solution here
+def list_sum(a,b):
+    i=0
+    for item in a:
+        a[i]=a[i]+b[i]
+        i=i+1
+    return a
+if __name__ == "__main__":
+    a = [1, 2, 3]
+    b = [7, 8, 9]
+    print(list_sum(a, b)) 
