e0290099a68ae1ef13e78e32b6c8887aaddc5f81
Rozina Karki <rozina.karki07@gmail.com>
2026-08-21 23:03:58 +0545

    added all _longes_in_list.py

--- mooc-programming-26/part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ mooc-programming-26/part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,1 +1,20 @@
-# Write your solution here
+def all_the_longest(my_list):
+    longest_words = []
+    max_len = 0
+    
+    for item in my_list:
+        current_len = len(item)
+        
+        if current_len > max_len:
+            max_len = current_len
+            longest_words = [item]
+            
+        elif current_len == max_len and max_len > 0:
+            longest_words.append(item)
+            
+    return longest_words
+if __name__ == "__main__":
+    my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
+
+    result = all_the_longest(my_list)
+    print(result) # ['dorothy', 'richard']
