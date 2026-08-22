1897bfd5e9e568772e7c14f44ec3aef12975f430
Rozina Karki <rozina.karki07@gmail.com>
2026-08-22 22:21:30 +0545

    added no vowels allowed

--- mooc-programming-26/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ mooc-programming-26/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,19 @@
-# Write your solution here
+def no_vowels(str):
+    vowel = "aeiou"
+    for item in vowel:
+            str = str.replace(item,"")
+    return str
+if __name__ == "__main__":
+    my_string = "this is an example"
+    print(no_vowels(my_string))
+
+
+# def no_vowels(str):
+#     str1 = str
+#     for item in str1:
+#         if item == "a" or item == "i" or item == "e" or item == "o" or item == "u":
+#             str = str.replace(item,"")
+#     return str
+# if __name__ == "__main__":
+#     my_string = "this is an example"
+#     print(no_vowels(my_string))
