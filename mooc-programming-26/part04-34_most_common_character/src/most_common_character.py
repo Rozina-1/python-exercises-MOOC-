bc3230f6f21086cc7a95428cdab3d0f204306039
Rozina Karki <rozina.karki07@gmail.com>
2026-08-22 22:01:40 +0545

    added most common character

--- mooc-programming-26/part04-34_most_common_character/src/most_common_character.py
+++ mooc-programming-26/part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,14 @@
-# Write your solution here
+def most_common_character(str):
+    temp = 0
+    st = ""
+    for item in str:
+        if str.count(item)>temp:
+            temp=str.count(item)
+            st=item
+    return st
+if __name__ == "__main__":
+    first_string = "abcdbde"
+    print(most_common_character(first_string))
+
+    second_string = "exemplaryelementary"
+    print(most_common_character(second_string))
