4539245179e62d383d76218363beb02b079edcf5
Rozina Karki <rozina.karki07@gmail.com>
2026-08-24 08:01:09 +0545

    added no shouting.py

--- mooc-programming-26/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
+++ mooc-programming-26/part04-36_no_shouting_allowed/src/no_shouting_allowed.py
@@ -1,1 +1,12 @@
-# Write your solution here
+def no_shouting(my_list):
+    new = []
+    for item in my_list:
+        if item.isupper():
+            continue
+        else:
+            new.append(item)
+    return new
+if __name__ == "__main__":
+    my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
