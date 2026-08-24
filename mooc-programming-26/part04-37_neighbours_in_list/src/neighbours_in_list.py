79ea1840bec8f3079a9d4340fc8632d3ed437159
Rozina Karki <rozina.karki07@gmail.com>
2026-08-24 08:30:03 +0545

    added longest series of neighbours.py

--- mooc-programming-26/part04-37_neighbours_in_list/src/neighbours_in_list.py
+++ mooc-programming-26/part04-37_neighbours_in_list/src/neighbours_in_list.py
@@ -1,1 +1,17 @@
-# Write your solution here
+def longest_series_of_neighbours(my_list):
+    count = 0
+    val = 0
+    
+    for i in range(len(my_list) - 1):
+        if my_list[i] - my_list[i+1] == 1 or my_list[i] - my_list[i+1] == -1:
+            count = count + 1
+        else:
+            if count > val:
+                val = count
+            count = 0
+    val = max(val, count)
+    return val + 1 if val > 0 else 1
+if __name__ == "__main__":
+    my_list = [1, 2, 5, 7, 6, 5, 6, 3, 4, 1, 0]
+    print(longest_series_of_neighbours(my_list))  
+
