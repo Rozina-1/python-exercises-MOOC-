a6fbd155a8eeccf4ce04ed02b5d48cb59b75950a
Rozina Karki <rozina.karki07@gmail.com>
2026-08-31 18:07:20 +0545

    added sudoku_column.py

---
+++ mooc-programming-26/part05-05_sudoku_column/src/sudoku_column.py
@@ -0,0 +1,22 @@
+def column_correct(sudoku: list, column_no: int):
+    new_row = []
+    for row in sudoku:
+        if row[column_no] > 0 and row[column_no] in new_row:
+            return False
+        new_row.append(row[column_no])
+    return True
+if __name__ == "__main__":
+    sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2]
+    ]
+
+    print(column_correct(sudoku, 0))
+    print(column_correct(sudoku, 1))
