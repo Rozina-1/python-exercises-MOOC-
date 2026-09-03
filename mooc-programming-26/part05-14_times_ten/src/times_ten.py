165d28924ac2b360686fca5d30e7e73a0d2ba0fe
Rozina Karki <rozina.karki07@gmail.com>
2026-09-03 10:22:23 +0545

    added times ten using dictionary function

---
+++ mooc-programming-26/part05-14_times_ten/src/times_ten.py
@@ -0,0 +1,9 @@
+def times_ten(start_index, end_index):
+    my_dictionary = {}
+    for i in range(start_index, end_index + 1):
+        my_dictionary[i] = i * 10
+    return my_dictionary
+
+if __name__ == "__main__":
+    d = times_ten(3,6)
+    print(d)
