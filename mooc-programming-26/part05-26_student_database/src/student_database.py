03b043f016b125cebaca7f5531d671b6213c7d90
Rozina Karki <rozina.karki07@gmail.com>
2026-09-05 16:09:40 +0545

    added part 3 for studnet database

--- mooc-programming-26/part05-26_student_database/src/student_database.py
+++ mooc-programming-26/part05-26_student_database/src/student_database.py
@@ -20,15 +20,21 @@ def print_student(students: dict, name: str):
              
 
 def add_course(students: dict, name: str, course: tuple):
+        if course[1] == 0:
+            return
+        for i,(cour, grade) in enumerate(students[name]):
+            if cour == course[0]:
+                if course[1] > grade:
+                    students[name][i] = course
+                return
+
         students[name].append(course)
 
 if __name__ == "__main__":  
     students = {}
     add_student(students, "Peter")
-    add_student(students, "Eliza")
-    print_student(students, "Peter")
-    print_student(students, "Eliza")
-    print_student(students, "Jack")
     add_course(students, "Peter", ("Introduction to Programming", 3))
     add_course(students, "Peter", ("Advanced Course in Programming", 2))
+    add_course(students, "Peter", ("Data Structures and Algorithms", 0))
+    add_course(students, "Peter", ("Introduction to Programming", 2))
     print_student(students, "Peter")
