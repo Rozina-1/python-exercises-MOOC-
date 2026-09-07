636a6620c77b90c3d5d2970f0582fe53eb95937b
Rozina Karki <rozina.karki07@gmail.com>
2026-09-07 13:18:18 +0545

    updates largest number

--- mooc-programming-26/part06-01_largest_number/src/largest_number.py
+++ mooc-programming-26/part06-01_largest_number/src/largest_number.py
@@ -1,10 +1,15 @@
+import os
+
 def largest():
-    with open("numbers.txt") as new_file:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(script_dir, "numbers.txt")
+    with open(file_path) as new_file:
         greatest = 0
         for line in new_file:
-            number1 = int(line.replace("\n",""))
+            number1 = int(line.replace("\n", ""))
             if number1 > greatest:
                 greatest = number1
     return greatest
+
 if __name__ == "__main__":
-    largest()
+    print(largest())
