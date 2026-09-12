8bc0a98a24c8e4321e7934a0b489a3fa64b36804
Rozina Karki <rozina.karki07@gmail.com>
2026-09-12 13:10:15 +0545

    added spellchecker.py

--- mooc-programming-26/part06-07_spellchecker/src/spellchecker.py
+++ mooc-programming-26/part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,16 @@
-# write your solution here
+wordlist = []
+from pathlib import Path
+
+script_dir = Path(__file__).parent
+file_path = script_dir / "wordlist.txt"
+with open(file_path) as new_file:
+    for line in new_file:
+        line = line.replace("\n","")
+        wordlist.append(line)
+
+sentence = input("Write text: ")
+text = sentence.split(" ")
+for word in text:
+    if word.lower() not in wordlist:
+        word = f"*{word}*"
+    print(word,end = " ")
