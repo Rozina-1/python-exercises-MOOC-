e1b9d678c0058815436625df74904a92f835d00e
Rozina Karki <rozina.karki07@gmail.com>
2026-09-13 21:51:51 +0545

    added recipe_search (part 1)

--- mooc-programming-26/part06-08_recipe_search/src/recipe_search.py
+++ mooc-programming-26/part06-08_recipe_search/src/recipe_search.py
@@ -1,1 +1,26 @@
-# Write your solution here
+
+def search_by_name(filename: str, word: str):
+    from pathlib import Path
+    script_dir = Path(__file__).parent
+    file_path = script_dir/filename
+    item = []
+    found_recipie = []
+    temprecipie = []
+    with open(file_path) as new_file:
+        for line in new_file:
+            line = line.strip("\n")
+            if line == "":
+                item.append(temprecipie)
+                temprecipie = []
+            else:
+                temprecipie.append(line)
+        item.append(temprecipie)
+    for block in item:
+        if word.lower() in block[0].lower():
+            found_recipie.append(block[0])
+    return found_recipie
+
+if __name__ == "__main__":
+    found_recipes = search_by_name("recipes1.txt", "cake")
+    for recipe in found_recipes:
+        print(recipe)
