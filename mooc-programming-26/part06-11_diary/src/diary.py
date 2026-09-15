09bdaf9832449bf0b482fb473d3f2ecb2f57936c
Rozina Karki <rozina.karki07@gmail.com>
2026-09-15 19:28:21 +0545

    added diary.py, (appending a file)

--- mooc-programming-26/part06-11_diary/src/diary.py
+++ mooc-programming-26/part06-11_diary/src/diary.py
@@ -1,1 +1,15 @@
-# Write your solution here
+while True:
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+    choice = int(input("Function: "))
+    if choice == 1:
+        with open("diary.txt", "a") as new_file:
+            diary_entry = input("Diary entry: ")
+            new_file.write(diary_entry + "\n")
+            print("Diary saved")
+    elif choice == 2:
+        with open("diary.txt") as new_file:
+            for line in new_file:
+                print(line)
+    elif choice == 0:
+        print("Bye now!")
+        break
