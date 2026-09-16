0c3ad67dbce0a9599e2cd7e781b2852696ebee9a
Rozina Karki <rozina.karki07@gmail.com>
2026-09-16 21:52:28 +0545

    added filtering_file_contents.py

--- mooc-programming-26/part06-12_filtering_file_contents/src/filtering_file_contents.py
+++ mooc-programming-26/part06-12_filtering_file_contents/src/filtering_file_contents.py
@@ -1,1 +1,22 @@
-# Write your solution here
+def filter_solutions():
+    incorrect = []
+    correct = []
+    with open("solutions.csv") as new_file:
+        for line in new_file:
+            line = line.strip("\n").split(";")
+            if eval(line[1]) != int(line[2]):
+                incorrect.append(f"{line[0]};{line[1]};{line[2]}")
+            else:
+                correct.append(f"{line[0]};{line[1]};{line[2]}")   
+
+    with open("correct.csv","w") as new_file:
+        for line in correct:
+            new_file.write(line + "\n")
+
+    with open("incorrect.csv","w") as new_file:
+        for line in incorrect:
+            new_file.write(line + "\n")
+
+if __name__ == "__main__":
+    filter_solutions()
+        
